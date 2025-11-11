a97e4be8eb11a4a7089eea6000d8466e0eab2aec
carlosjeferson <Carlosjjs500214@gmail.com>
2025-11-11 11:00:01 -0300

    feat(frontend): Implementei telas frontend

--- frontend/js/reports.js
+++
@@ -1,7 +0,0 @@
-async function loadReports() {
-  const data = await apiGet("schedule");
-  const total = data.length;
-  document.getElementById("reports").innerHTML = `
-    <p>Total de estudos registrados: ${total}</p>
-  `;
-}
